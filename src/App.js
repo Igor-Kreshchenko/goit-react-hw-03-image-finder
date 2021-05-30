df9322153fa8894beb1a1ce6f5528b6d37c0a8f7
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-05-30 12:27:42 +0300

    Подключает React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Section from './components/Section';
+import styles from './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
